520ca0240a10855bcfee7f0e366d4beb4357c9c8
unknown <wesleyfreire1707@gmail.com>
2024-12-01 21:14:08 -0300

    Alteração

---
+++ models.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host ='localhost',
+    user='root',
+    password='Wes170702',
+    database='cadastro_cliente',
+)
+
+cursor = conexao.cursor()
+
+cursor.close()
+conexao.close()
